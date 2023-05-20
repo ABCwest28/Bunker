9e64f84383cd85418944b528136daeedc7b08578
ABCwest28 <ABCwest28@gmail.com>
2023-05-21 01:45:37 +0500

    Работает стартовое окошко Недоделано

--- Bunker_client.py
+++ Bunker_client.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
-from PyQt5 import QtGui
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QWidget, QLineEdit)
+from PyQt5.QtCore import QRegExp
+from PyQt5.QtGui import QRegExpValidator
 
 
 class BunkerClientStartWindow(QWidget):
@@ -9,14 +10,40 @@ class BunkerClientStartWindow(QWidget):
     """
     def __init__(self):
         super().__init__()
-        layout = QVBoxLayout()
-        self.label = QLabel("Another Window")
-        layout.addWidget(self.label)
-        self.setLayout(layout)
+
+        self.h_layout_1 = QHBoxLayout()
+        self.h_layout_2 = QHBoxLayout()
+        self.v_layout_1 = QVBoxLayout()
+
+        self.label_1 = QLabel("Никнейм:", self)
+        self.label_2 = QLabel("Ip-адрес сервера:", self)
+        self.line_edit_nik = QLineEdit(self)
+        self.line_edit_ip = QLineEdit(self)
+
+        self.initUi()
         self.show()
 
-    def closeEvent(self, a0: QtGui.QCloseEvent):
-        client_window.show()
+    def initUi(self):
+        self.setWindowTitle('Bunker Client')
+        self.initUi_line_edit_ip_validation()
+        self.initUi_layouts()
+
+    def initUi_line_edit_ip_validation(self):
+        ipRange = "(?:[0-1]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])"                                           # Часть регулярного выржение
+        ipRegex = QRegExp("^" + ipRange + "\\." + ipRange + "\\." + ipRange + "\\." + ipRange + "$")    # Само регулярное выражение
+        ipValidator = QRegExpValidator(ipRegex, self)                                                   # Валидатор для QLineEdit
+        self.line_edit_ip.setValidator(ipValidator)
+
+    def initUi_layouts(self):
+        self.h_layout_1.addWidget(self.label_1)
+        self.h_layout_1.addWidget(self.line_edit_nik)
+
+        self.h_layout_2.addWidget(self.label_2)
+        self.h_layout_2.addWidget(self.line_edit_ip)
+
+        self.v_layout_1.addLayout(self.h_layout_1)
+        self.v_layout_1.addLayout(self.h_layout_2)
+        self.setLayout(self.v_layout_1)
 
 
 class BunkerClientMainWindow(QMainWindow):
@@ -33,18 +60,8 @@ class BunkerClientMainWindow(QMainWindow):
         self.w.show()
         self.close()
 
-    def show_new_window_2(self, checked):
-        """Этот метод аналог 1го"""
-        if self.w is None:
-            self.w = AnotherWindow()
-            self.w.show()
-
-        else:
-            self.w.close()  # Close window.
-            self.w = None  # Discard reference.
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    client_window = BunkerClientMainWindow()
+    client_window = BunkerClientStartWindow()
     sys.exit(app.exec_())
